8dd3f6d75fd580c1c54665668c4abb7037f8c046
svetlana <svetlana.syo@gmail.com>
2020-01-07 16:30:45 +0100

    Implement Interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/learning/project/sfgpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package learning.project.sfgpetclinic.services;
+
+import learning.project.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
